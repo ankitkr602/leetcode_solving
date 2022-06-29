c6dd69ff4ede917fe1c1989a965e3d13f7340542
ankitkr602 <37143800+ankitkr602@users.noreply.github.com>
2022-06-29 23:12:29 +0530

    Time: 43 ms (84.48%), Space: 19.1 MB (46.12%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -2,30 +2,23 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>>v;
-        vector<int>v1(2);
-        pair<int,int>range;
+        vector<int>range;
         sort(intervals.begin(),intervals.end());
-        //for(int i=0;i<)
-        range=make_pair(intervals[0][0],intervals[0][1]);
+        range=intervals[0];
         for(int i=0;i<intervals.size();i++)
         {
-            if(intervals[i][0]<=range.second)
+            if(intervals[i][0]<=range[1])
             {
-                range.first=min(range.first,intervals[i][0]);
-                range.second=max(range.second,intervals[i][1]);
+                range[0]=min(range[0],intervals[i][0]);
+                range[1]=max(range[1],intervals[i][1]);
                 
             }
             else{
-                v1[0]=(range.first);
-                v1[1]=(range.second);
-                v.push_back(v1);
-                range.first=intervals[i][0];
-                range.second=intervals[i][1];
+                v.push_back(range);
+                range=intervals[i];
             }
         }
-        v1[0]=(range.first);
-                v1[1]=(range.second);
-                v.push_back(v1);
+        v.push_back(range);
         return v;
         
     }
