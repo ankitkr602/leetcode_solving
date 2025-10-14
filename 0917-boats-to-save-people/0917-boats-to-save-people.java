73dd5951505225809add86bac303e94328ce57c7
ankitkr602 <37143800+ankitkr602@users.noreply.github.com>
2025-10-14 07:32:12 +0530

    Time: 16 ms (94.88%), Space: 55 MB (68.27%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.java
+++ 0917-boats-to-save-people/0917-boats-to-save-people.java
@@ -3,19 +3,16 @@ class Solution {
         Arrays.sort(people);
         int boat =0;
         int left =0, right = people.length -1;
-        while(left < right){
+        while(left <= right){
             if(people[left]+people[right]<= limit){
-            boat++;
             left++;
             right--;
         }
         else if(people[left]+people[right] > limit){
-            boat++;
             right--;
         }
+        boat++;
     }
-        if(left==right)
-        return boat+1;
-        else return boat;
+        return boat;
     }
 }
