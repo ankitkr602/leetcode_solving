d779af2f0a35fe0467f5efa6105c0b0bb9d3d46a
ankitkr602 <37143800+ankitkr602@users.noreply.github.com>
2025-10-25 04:07:00 +0530

    Time: 29 ms (6.42%), Space: 45.6 MB (15.32%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean bananaEating(int[] piles, int k, int h){
+        int time = 0;
+        for(int pile :  piles){
+            time += Math.ceil((double)pile/k);
+        }
+        if(time <= h)
+        return true;
+        return false;
+        
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int mineating = Integer.MAX_VALUE;
+        Arrays.sort(piles);
+        int left = 1;
+        int right = piles[piles.length -1];
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(bananaEating(piles,mid,h)){
+                right = mid -1;
+                mineating = Math.min(mid, mineating);
+            }
+            else{
+                left = mid+1;
+            }
+        }
+        return mineating;
+    }
+}
