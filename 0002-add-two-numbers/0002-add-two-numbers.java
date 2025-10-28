67a87203bc67c0c1ff810bb7697c010409b1d468
ankitkr602 <37143800+ankitkr602@users.noreply.github.com>
2025-10-29 02:12:24 +0530

    Time: 1 ms (100%), Space: 44.3 MB (93.95%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -14,42 +14,22 @@ class Solution {
         ListNode result = head;
         int carry = 0;
         int sum =0;
-        while(l1!=null && l2!=null){
-            sum = l1.val + l2.val + carry;
+        while(l1!=null || l2!=null || carry!=0){
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            sum = x + y + carry;
             int digit = sum%10;
             sum = sum/10;
             carry = sum%10;
             ListNode temp = new ListNode(digit, null);
             result.next =temp;
             result = result.next;
+            if(l1 != null)
             l1 = l1.next;
+            if(l2 != null)
             l2 = l2.next;
         }
-        while(l1 == null && l2!=null){
-            sum = l2.val + carry;
-            int digit = sum%10;
-            sum = sum/10;
-            carry = sum%10;
-            ListNode temp = new ListNode(digit, null);
-            result.next =temp;
-            result = result.next;
-            l2 = l2.next;
-        }
-        while(l2 == null && l1!=null){
-            sum = l1.val + carry;
-            int digit = sum%10;
-            sum = sum/10;
-            carry = sum%10;
-            ListNode temp = new ListNode(digit, null);
-            result.next =temp;
-            result = result.next;
-            l1 = l1.next;
-        }
-        if(carry> 0){
-            ListNode temp = new ListNode(carry, null);
-             result.next =temp;
-             result = result.next;
-        }
+        
         return head.next;
     }
 }
